ac3ca3d18d626b5ff826765857d5a95686417ea8
Thanh Loi <marcomoi395@gmail.com>
2024-12-02 13:30:26 +0700

    feat: implement API key with permission system

--- src/routers/index.js
+++ src/routers/index.js
@@ -1,7 +1,13 @@
 'use strict'
 
 const express = require('express')
+const {apiKey, permission} = require("../auth/checkAuth");
 const router = express.Router();
+
+// Check API
+router.use(apiKey)
+// Check Permission
+router.use(permission('0000'))
 
 router.use('/v1/api', require('./access'));
 
